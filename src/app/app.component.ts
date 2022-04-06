ffafa014be78ec201f1c0af6c422d515a722c1b5
Karenkoech <karenkoech3@gmail.com>
2022-04-06 17:14:23 -0400

    add all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Goal } from './goal';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'karenapp';
-}
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  karenapp:Goal[]=
+  [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
+
+  // constructor(){
+  //   this.karenapp=["watch movies","read more","read again"]
+  }
+
